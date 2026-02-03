668c3ca5cddb53cf3da5ddfcda2b02b775296403
Chhavi Gupta <chhaveygupta@gmail.com>
2026-02-03 14:57:59 +0530

    add zon again

--- backend/src/models/Session.js
+++ backend/src/models/Session.js
@@ -6,7 +6,7 @@ const sessionSchema = new mongoose.Schema(
     visitor: { type: mongoose.Schema.Types.ObjectId, ref: "Visitor", required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     url: { type: String },
-    events: { type: [Object], default: [] },
+    events: { type: [String], default: [] },
   },
   { timestamps: true },
 );
